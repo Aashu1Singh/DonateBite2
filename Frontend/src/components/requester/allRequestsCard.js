4e632664e6150c28d06fdd49a83d1ed44091b177
khanshahnwaz <shahnwazkhan2007@gmail.com>
2025-04-27 01:46:16 +0530

    status udpdate

--- Frontend/src/components/requester/allRequestsCard.js
+++ Frontend/src/components/requester/allRequestsCard.js
@@ -11,6 +11,7 @@ export default function allRequestsCard({ request }) {
                     <p class="card-text">
                         {request.description}
                     </p>
+                    <p className='bg-gray-400 px-2 py-2'>{request.status==="started"?"Active":"Distributed"}</p>
                 </div>
             </div>
         </div>
